cfd8d6d32a2471db114a0851e06fdc7f4c1f39c6
codingdayo <145878788+codingdayo@users.noreply.github.com>
2024-07-21 01:58:48 +0100

    first commit

---
+++ src/main/java/com/codingdayo/product_service/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.codingdayo.product_service.repository;
+
+import com.codingdayo.product_service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
